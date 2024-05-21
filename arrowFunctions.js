cda26e9217340802684b2630f9f1a6eb4991cc31
Swathi-Rajendran <swathiraj490@gmail.com>
2024-05-21 14:58:00 +0530

    arrowFunctions.js

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -1,3 +1,6 @@
-const arrowsFunction = () => {
-  console.log("HERE LOGGING THE ARROW FUNCTIONS");
+const add = (num1, num2, num3) => {
+  let result = num1 + num2 + num3;
+  return result;
 };
+add(1230, 3402, 5612);
+
